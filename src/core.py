81ecc08b6bda5cca959e6c1b7e0a26552ddabb65
wjdghcks6735 <wjdghcks02@gmail.com>
2024-02-14 18:54:19 +0900

    feat [ADD] auto backward

--- src/core.py
+++ src/core.py
@@ -2,12 +2,26 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+    
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
+        self.input = input
+        self.output = output
         return (output)
     
     def forward(self, x):
